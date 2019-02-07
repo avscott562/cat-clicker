7d2236ec7b2fe20cdea0c6114a1430537ad09cef
aliciascott <avscott@pacbell.net>
2019-02-06 21:22:27 -0800

    add restart button with function and styling

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 let click = document.querySelector('.click-counter');
 let clickCount = 0;
 let cat = document.querySelector('.tom-cat-image');
+let restart = document.querySelector('.restart');
 
 cat.addEventListener('click', function() {
   clickCount++;
@@ -8,5 +9,10 @@ cat.addEventListener('click', function() {
     click.innerHTML = clickCount + " Click";
   } else {
     click.innerHTML = clickCount + " Clicks";
-  };  
+  };
+}, false);
+
+restart.addEventListener('click', function() {
+  clickCount = 0;
+  click.innerHTML = "0 Clicks";
 });
